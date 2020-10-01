420bb3668e93f6d10a1a9dbf5c5ad79f81d5c54d
Iheb Rejeb <iheb.rejeb@esprit.tn>
2020-10-01 21:20:19 +0100

    template nextjs mongoose tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    future: {
+        removeDeprecatedGapUtilities: true,
+    },
+    purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+
+        },
+    },
+    variants: {},
+    plugins: [],
+}
